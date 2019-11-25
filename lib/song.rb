4dc4c89ee03bfc3f1c95aa643fa0dab734bf0f5f
Shahwar Spall <shevy.spall@gmail.com>
2019-11-25 05:44:19 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,23 @@
-tesy
+require "pry"
+class Song 
+  attr_accessor :name, :artists,:genres
+  
+@@count = 0
+
+def initialize(name,artist,genre)
+@@count += 1 
+
+end 
+
+def self.genres 
+  @@genres =[]
+  
+end 
+
+def self.artists 
+  @@artists 
+  
+ # binding.pry 
+end
+
+end 
