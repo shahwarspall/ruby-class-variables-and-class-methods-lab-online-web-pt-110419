4c51543a2394489f99cffc377052e54dd605f2aa
Shahwar Spall <shevy.spall@gmail.com>
2019-11-26 05:41:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,23 +1,39 @@
 require "pry"
 class Song 
-  attr_accessor :name, :artists,:genres
+  attr_accessor :name, :artist,:genre
   
 @@count = 0
+@@genres =[]
+@@artists =[]
 
 def initialize(name,artist,genre)
-@@count += 1 
-
-end 
-
-def self.genres 
-  @@genres =[]
-  
+  @name = name 
+  @@count += 1 
+  @genre=genre 
+  @artist=artist
+  @@genres << genre 
+  @@artists << artist
+#binding.pry
 end 
 
 def self.artists 
-  @@artists 
+  @@artists.uniq
+end 
+
+def self.genres 
+  @@genres
+end 
+def self.count 
+  @@count 
   
  # binding.pry 
+end 
+
+
+def self.artists
+  @@artists
+  
+ #binding.pry 
 end
 
 end 
